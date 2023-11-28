f5f407221bc6675c19e746683bf4b911a3fe51ee
alex02r <alex.giochi02@gmail.com>
2023-11-28 15:00:51 +0100

    [UP]Probabile soluzione al problema 1

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/* 
+    SOLUZIONE PALINDROMA
+    1 - Chiedere all'utente di inserire una parola.
+    2 - Creiamo una funzione ChechkPalindroma(parola) e gli passiamo come parametro la parola che abbiamo inserito in precedenza.
+        3 - invertiamo la parola con la funzione .slpit('').reverse('').join(''), e la inseriamo nell'array;
+        4.A - ? la parola invertita == parola inserita
+            5 - Allora la parola è palindroma return true;
+        6 - return false (possiamo mettere il return anche fuori dall if, perchè se entra trova il primo return ed esce.)
+
+*/
